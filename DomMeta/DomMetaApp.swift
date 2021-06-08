437b2346ed8ad25403450d5e3be14f1422f1c35e
Daniel Watson <55237431+ddddeano@users.noreply.github.com>
2021-06-08 13:20:11 +0200

    Add files via upload

---
+++ DomMeta/DomMetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomMetaApp.swift
+//  DomMeta
+//
+//  Created by Daniel Watson on 08/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DomMetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
